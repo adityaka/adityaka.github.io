da54ad1fbfa330729b526e42ff589988e148211f
Aditya Kapur <adityaka@live.com>
2025-06-03 09:56:28 +0000

    Changed the RUM Tag version from latest to 4.0.9

--- javascripts/rum.js
+++ javascripts/rum.js
@@ -1,6 +1,7 @@
 
 var rumScript = document.createElement("script")
-var url = "https://cpqa.catchpoint.com/jp/191947/latest/InitialLoadScript.js"
+// var url = "https://cpqa.catchpoint.com/jp/191947/latest/InitialLoadScript.js"
+var url = "https://cpqa.catchpoint.com/jp/191947/v4.0.9/InitialLoadScript.js"
 rumScript.src = url;
 rumScript.defer = true;
 document.head.appendChild(rumScript);
